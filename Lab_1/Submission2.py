578b37c777c7b2af0a9f47def51304112c84fef3
SudhirAjodhya <sudhir.ajodhya@gmail.com>
2025-08-04 12:52:10 +0200

    Updated Lab2 sub 2

--- Lab_1/Submission2.py
+++ Lab_1/Submission2.py
@@ -1,19 +1,6 @@
 from enum import Enum
 import math
 
-class Move(Enum):
-    UP = (-1, 0)
-    DOWN = (1, 0)
-    LEFT = (0, -1)
-    RIGHT = (0, 1)
-
-def string_to_board(board):
-    iterator = 0
-    for row in range(3):
-        for col in range(3):
-            board[row][col] = board_input[iterator]
-            iterator+= 1
-
 def available_moves(row, col):
     avail_moves = []
      
@@ -24,8 +11,6 @@ def available_moves(row, col):
     return avail_moves
 
 board_input = input()
-board = [[0, 0, 0], [0, 0, 0], [0, 0,0]]
-string_to_board(board)
 
 hash_position = board_input.find("#")
 hash_pos_row = math.floor(hash_position / 3)
